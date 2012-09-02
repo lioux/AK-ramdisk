de61bfbc487406c5171800e3741215b4995f9dfe
anarkia1976 <stefano.villa1976@gmail.com>
2012-09-02 14:17:06 +0200

    Rework Style

--- ramdisk-cm10/sbin/akconfiguration.sh
+++ ramdisk-cm10/sbin/akconfiguration.sh
@@ -16,44 +16,22 @@ echo "0" > /sys/module/alarm/parameters/debug_mask
 echo "0" > /sys/module/alarm_dev/parameters/debug_mask
 echo "0" > /sys/module/binder/parameters/debug_mask
 
-$bb mount -o rw,remount /system
 $bb echo ""
 $bb echo "Fix power.tuna.so Lib -----------------------------------"
 $bb date >>$log
-
-$bb cp -vr /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.bak
-$bb cp -vr /sbin/files/hw/power.tuna.so /system/lib/hw
-$bb chmod 644 /system/lib/hw/power.tuna.so
-
 $bb echo "List .so libraries:"
 $bb ls -l /system/lib/hw/
-
 $bb date >>$log
 $bb echo "End -----------------------------------------------------"
 $bb echo ""
-$bb mount -o ro,remount /system
 
-$bb mount -o rw,remount /system
 $bb echo ""
 $bb echo "Copy modules Lib ----------------------------------------"
 $bb date >>$log
-
-if $bb [ ! -d /system/lib/modules ]; then   
-    $bb echo "Module Directory not found ... Making it ..."
-    $bb mkdir /system/lib/modules
-    $bb chmod 777 /system/lib/modules
-fi
-
-$bb rm -rf /system/lib/modules/*
-$bb cp -vr /sbin/files/modules/* /system/lib/modules/
-$bb chmod 644 /system/lib/modules/*
-
 $bb echo "List .ko libraries:"
 $bb ls -l /system/lib/modules/
-
 $bb date >>$log
 $bb echo "End -----------------------------------------------------"
 $bb echo ""
-$bb mount -o ro,remount /system
 
 exit
