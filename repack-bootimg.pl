d1ca29b2ab6398a0816feb8d43bb5c0bf34eb082
anarkia1976 <stefano.villa1976@gmail.com>
2012-09-13 12:02:56 +0200

    LZO Support

--- repack-bootimg.pl
+++ repack-bootimg.pl
@@ -12,12 +12,12 @@ die $usage unless $ARGV[0] && $ARGV[1] && $ARGV[2];
 
 chdir $ARGV[1] or die "$ARGV[1] $!";
 
-system ("find . | cpio -o -H newc | gzip > $dir/ramdisk-repack.cpio.gz");
+system ("find . | cpio -o -H newc | lzop > $dir/ramdisk-repack.cpio.lzo");
 
 chdir $dir or die "$ARGV[1] $!";;
 
-system ("./mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.gz -o $ARGV[2]");
+system ("./mkbootimg --cmdline 'no_console_suspend=1 console=null' --kernel $ARGV[0] --ramdisk ramdisk-repack.cpio.lzo -o $ARGV[2]");
 
-unlink("ramdisk-repack.cpio.gz") or die $!;
+unlink("ramdisk-repack.cpio.lzo") or die $!;
 
 print "\nrepacked boot image written at $ARGV[1]-repack.img\n";
