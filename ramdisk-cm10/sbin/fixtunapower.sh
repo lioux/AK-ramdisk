96b347a577480f49d8c5be446c5599335a330567
anarkia1976 <stefano.villa1976@gmail.com>
2012-09-20 08:48:56 +0200

    Changes for Kernel Version: ak.067.jb.exp

--- ramdisk-cm10/sbin/fixtunapower.sh
+++ ramdisk-cm10/sbin/fixtunapower.sh
@@ -4,8 +4,8 @@ bb="/sbin/bb/busybox"
 $bb mount -o rw,remount /system
 $bb cp /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.bak
 $bb cp /system/lib/hw/power.default.so /system/lib/hw/power.default.so.bak
-#$bb rm -rf /system/lib/hw/power.tuna.so
-#$bb rm -rf /system/lib/hw/power.default.so
+$bb rm -rf /system/lib/hw/power.tuna.so
+$bb rm -rf /system/lib/hw/power.default.so
 #$bb cp /sbin/power.tuna.so /system/lib/hw
 #$bb chmod 644 /system/lib/hw/power.tuna.so
 $bb chmod 644 /system/lib/hw/power.tuna.so.bak
