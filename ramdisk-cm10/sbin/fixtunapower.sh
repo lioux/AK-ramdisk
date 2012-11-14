a07a85c2a453e10735799528e48570e00b734281
anarkia1976 <stefano.villa1976@gmail.com>
2012-11-14 12:16:28 +0100

    fixtunapower.sh: Add init.d creation

--- ramdisk-cm10/sbin/fixtunapower.sh
+++ ramdisk-cm10/sbin/fixtunapower.sh
@@ -3,6 +3,14 @@ bb="/sbin/bb/busybox"
 
 $bb mount -o rw,remount /system
 
+if $bb [ ! -d /system/etc/init.d ]; then
+ $bb echo "Making Init.d Directory ..."
+ $bb mkdir /system/etc/init.d
+ $bb chmod 777 /system/etc/init.d
+else
+ $bb echo "Init.d Directory Exist ..."
+fi
+
 if [ ! -e /system/lib/hw/power.tuna.so.bak ]; then
  $bb cp /system/lib/hw/power.tuna.so /system/lib/hw/power.tuna.so.bak
 fi
