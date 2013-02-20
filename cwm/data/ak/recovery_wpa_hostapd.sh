0301625b90c28e832daaf17058cc7f7ae05afd69
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-20 12:20:44 +0100

    recovery_wpa_hostapd.sh: Add rw,remount /system

--- cwm/data/ak/recovery_wpa_hostapd.sh
+++ cwm/data/ak/recovery_wpa_hostapd.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 # Script for Recovery 4.2.2 Wpa and Hostapd
 
+mount -o rw,remount /system
+
 echo "Coping Files to System Dir ..."
 
 cp -vr /data/ak/backup/hostapd /system/bin
@@ -9,3 +11,5 @@ cp -vr /data/ak/backup/wpa_supplicant /system/bin
 cp -vr /data/ak/backup/wpa_cli /system/bin
 
 echo "Recovery Done ..."
+
+mount -o ro,remount /system
