f35cf58473d32e0c5fb0d0667cd7f3ead6a3788c
anarkia1976 <stefano.villa1976@gmail.com>
2013-02-20 12:37:10 +0100

    recovery_wpa_hostapd.sh: Add chown and chmod permissions

--- cwm/data/ak/recovery_wpa_hostapd.sh
+++ cwm/data/ak/recovery_wpa_hostapd.sh
@@ -10,6 +10,16 @@ cp -vr /data/ak/backup/hostapd_cli /system/bin
 cp -vr /data/ak/backup/wpa_supplicant /system/bin
 cp -vr /data/ak/backup/wpa_cli /system/bin
 
+chown root:shell /system/bin/hostapd
+chown root:shell /system/bin/hostapd_cli
+chown root:shell /system/bin/wpa_supplicant
+chown root:shell /system/bin/wpa_cli
+
+chmod 755 /system/bin/hostapd
+chmod 755 /system/bin/hostapd_cli
+chmod 755 /system/bin/wpa_supplicant
+chmod 755 /system/bin/wpa_cli
+
 echo "Recovery Done ..."
 
 mount -o ro,remount /system
